00d38a4ffa38d6f65b6d540ef527591230eae61f
Edgar Briceno <edgarbg585@gmail.com>
2023-08-25 23:06:41 -0700

    removed extra content

--- content.js
+++ content.js
@@ -16,25 +16,3 @@ function logAnchorHref(event) {
       element.addEventListener('mouseover', logAnchorHref);
     });
   }
-  
-  // Wait for DOM content to load
-  document.addEventListener('DOMContentLoaded', function() {
-    // Monitor for changes in the DOM using MutationObserver
-    const observer = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        // Check if new nodes are added to the DOM
-        if (mutation.addedNodes) {
-          mutation.addedNodes.forEach(node => {
-            // Check if the added node is an element
-            if (node.nodeType === Node.ELEMENT_NODE) {
-              node.addEventListener('mouseover', logAnchorHref);
-            }
-          });
-        }
-      });
-    });
-  
-    // Start observing the DOM for changes
-    observer.observe(document.body, { childList: true, subtree: true });
-  });
-  
